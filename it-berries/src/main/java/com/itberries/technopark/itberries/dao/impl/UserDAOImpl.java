9b83d5dc280da3f4624de7152632498e306c2021
elenaoshkina <oshkina95@gmail.com>
2019-05-21 04:20:27 +0300

    fix

--- it-berries/src/main/java/com/itberries/technopark/itberries/dao/impl/UserDAOImpl.java
+++ it-berries/src/main/java/com/itberries/technopark/itberries/dao/impl/UserDAOImpl.java
@@ -106,7 +106,7 @@ public class UserDAOImpl implements IUserDAO {
                 "(\n" +
                 "        (SELECT u.id, u.score, DENSE_RANK()  OVER (ORDER BY u.score DESC) AS rank\n" +
                 "         FROM users u \n" +
-                "         WHERE u.score > 0 AND u.id IN (:userIdsб :userId)\n" +
+                "         WHERE u.score > 0 AND u.id IN (:userIds, :userId)\n" +
                 "         LIMIT 10)\n" +
                 "        UNION\n" +
                 "        (\n" +
